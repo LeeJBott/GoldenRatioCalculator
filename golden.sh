e1d9af3c726695f1fe399940b1e7d8356b927394
Lee Bottomley <155629288+LeeJBott@users.noreply.github.com>
2024-02-20 23:20:51 +0000

    Update golden.sh

--- golden.sh
+++ golden.sh
@@ -1,1 +1,9 @@
-l
+#!/bin/bash
+echo -e "Setup Starting\n"
+sleep 1
+echo "Writing Alias file"
+sleep 2
+echo -e "\nalias golden='python3 /home/$USER/GoldenRatioCalculator/GoldenRatio.py'" >> ~/.bashrc
+source ~/.bashrc
+echo "File Updated, use the 'golden' command to run the calculator!"
+echo "Thanks"
